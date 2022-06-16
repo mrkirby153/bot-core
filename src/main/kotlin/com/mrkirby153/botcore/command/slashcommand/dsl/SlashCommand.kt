091484c4a029b09ba80e68cd9978ba15da981cf9
mrkirby153 <mr.austinwhyte@gmail.com>
2022-06-15 20:02:32 -0700

    Add command group DSL

--- src/main/kotlin/com/mrkirby153/botcore/command/slashcommand/dsl/SlashCommand.kt
+++ src/main/kotlin/com/mrkirby153/botcore/command/slashcommand/dsl/SlashCommand.kt
@@ -1,14 +1,19 @@
 package com.mrkirby153.botcore.command.slashcommand.dsl
 
+@SlashDsl
 class SlashCommand<A : Arguments>(
     private val arguments: (() -> A)? = null
 ) {
-    lateinit var body: Context<A>.() -> Unit
+    var body: (Context<A>.() -> Unit)? = null
     lateinit var name: String
     lateinit var description: String
 
+    val groups = mutableMapOf<String, List<SubCommand<*>>>()
 
     fun action(action: Context<A>.() -> Unit) {
+        if (groups.isNotEmpty()) {
+            throw IllegalArgumentException("Cannot mix groups and non-grouped commands")
+        }
         this.body = action
     }
 
@@ -21,9 +26,26 @@ class SlashCommand<A : Arguments>(
             }
         }
     }
+
     fun run() {
         val ctx = Context(args()!!)
     }
 
     fun args(): A? = arguments?.invoke()
+}
+
+@SlashDsl
+class Group {
+    val commands = mutableListOf<SubCommand<*>>()
+}
+
+@SlashDsl
+class SubCommand<A : Arguments>(private val arguments: (() -> A)? = null) {
+    lateinit var body: Context<A>.() -> Unit
+    lateinit var name: String
+    lateinit var description: String
+
+    fun action(action: Context<A>.() -> Unit) {
+        this.body = action
+    }
 }
--- src/main/kotlin/com/mrkirby153/botcore/command/slashcommand/dsl/_Commands.kt
+++ src/main/kotlin/com/mrkirby153/botcore/command/slashcommand/dsl/_Commands.kt
@@ -3,7 +3,10 @@ package com.mrkirby153.botcore.command.slashcommand.dsl
 @DslMarker
 annotation class SlashDsl
 
-fun <T : Arguments> slashCommand(arguments: () -> T, body: SlashCommand<T>.() -> Unit): SlashCommand<T> {
+fun <T : Arguments> slashCommand(
+    arguments: () -> T,
+    body: SlashCommand<T>.() -> Unit
+): SlashCommand<T> {
     val command = SlashCommand(arguments)
     body(command)
     return command
@@ -11,4 +14,23 @@ fun <T : Arguments> slashCommand(arguments: () -> T, body: SlashCommand<T>.() ->
 
 fun slashCommand(body: SlashCommand<Arguments>.() -> Unit): SlashCommand<Arguments> {
     return slashCommand(::Arguments, body)
+}
+
+fun SlashCommand<*>.group(name: String, body: Group.() -> Unit) {
+    val group = Group()
+    body(group)
+    if (this.groups[name] != null) {
+        throw IllegalArgumentException("Duplicate group $name")
+    }
+    this.groups[name] = group.commands
+}
+
+fun <T : Arguments> Group.slashCommand(arguments: () -> T, body: SubCommand<T>.() -> Unit) {
+    val cmd = SubCommand(arguments)
+    body(cmd)
+    this.commands.add(cmd)
+}
+
+fun Group.slashCommand(body: SubCommand<Arguments>.() -> Unit) {
+    slashCommand(::Arguments, body)
 }
