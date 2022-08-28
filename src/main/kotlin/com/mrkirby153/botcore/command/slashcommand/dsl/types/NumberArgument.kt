8bdc1e044473a67758f29a2d7b5daa64cb44f206
mrkirby153 <mr.austinwhyte@gmail.com>
2022-08-27 22:15:45 -0700

    Correctly set number min and max

--- src/main/kotlin/com/mrkirby153/botcore/command/slashcommand/dsl/types/NumberArgument.kt
+++ src/main/kotlin/com/mrkirby153/botcore/command/slashcommand/dsl/types/NumberArgument.kt
@@ -56,16 +56,31 @@ class DoubleArgumentBuilder(inst: Arguments) :
     NumberArgumentBuilder<Double>(inst, DoubleConverter) {
     override var min: Double = max(OptionData.MIN_NEGATIVE_NUMBER, Double.MIN_VALUE)
     override var max: Double = min(OptionData.MAX_POSITIVE_NUMBER, Double.MAX_VALUE)
+
+    override fun createOption() = super.createOption().apply {
+        setMaxValue(max)
+        setMinValue(min)
+    }
 }
 
 class LongArgumentBuilder(inst: Arguments) : NumberArgumentBuilder<Long>(inst, LongConverter) {
     override var min: Long = max(Long.MIN_VALUE, OptionData.MIN_NEGATIVE_NUMBER.toLong())
     override var max: Long = min(Long.MAX_VALUE, OptionData.MAX_POSITIVE_NUMBER.toLong())
+
+    override fun createOption() = super.createOption().apply {
+        setMaxValue(max)
+        setMinValue(min)
+    }
 }
 
 class IntegerArgumentBuilder(inst: Arguments) : NumberArgumentBuilder<Int>(inst, IntegerConverter) {
     override var min: Int = max(Int.MIN_VALUE, OptionData.MIN_NEGATIVE_NUMBER.toInt())
     override var max: Int = min(Int.MAX_VALUE, OptionData.MAX_POSITIVE_NUMBER.toInt())
+
+    override fun createOption() = super.createOption().apply {
+        setMaxValue(max.toLong())
+        setMinValue(min.toLong())
+    }
 }
 
 fun Arguments.double(body: DoubleArgumentBuilder.() -> Unit) =
